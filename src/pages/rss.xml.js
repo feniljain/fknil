e11226c832f04b092ed24d086164eaf2afc68f50
feniljain <fkjainco@gmail.com>
2026-05-24 12:41:29 +0530

    feat: send text MD content in RSS feed
    add: blogs to rgignore

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = (new MarkdownIt()).disable('image');
 
 export async function GET(context) {
     const blogs = (await getCollection('blogs', ({ data }) => !data.draft));
@@ -8,13 +11,16 @@ export async function GET(context) {
 
     return rss({
         title: 'Fenils Blog',
-        description: '',
+        description: 'What does it mean to be strong Takamura-san?',
         site: context.site,
         items: sortedBlogs.map((blog) => ({
-          title: blog.data.title,
-          pubDate: blog.data.publishedOn,
-          description: '',
-          link: `/blog/${blog.data.slug}/`,
+            title: blog.data.title,
+            pubDate: blog.data.publishedOn,
+            description: '',
+            link: `/blog/${blog.data.slug}/`,
+            content: sanitizeHtml(parser.render(blog.body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+            }),
         })),
     });
 }
