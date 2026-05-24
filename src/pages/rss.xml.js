08f11d8b14a2ba1ecdc20de2ebc569e4c867bbe6
feniljain <fkjainco@gmail.com>
2026-05-24 12:10:02 +0530

    fix: sort on publish date and remove drafts from RSS feed
    bump: blogs for claude head date fix

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,16 +2,19 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const blogs = await getCollection('blogs');
-  return rss({
-    title: 'Fenils Blog',
-    description: '',
-    site: context.site,
-    items: blogs.map((blog) => ({
-      title: blog.data.title,
-      pubDate: blog.data.publishedOn,
-      description: '',
-      link: `/blog/${blog.data.slug}/`,
-    })),
-  });
+    const blogs = (await getCollection('blogs', ({ data }) => !data.draft));
+
+    const sortedBlogs = (blogs.sort((a, b) => b.data.publishedOn.getTime() - a.data.publishedOn.getTime())).slice(0, 10);
+
+    return rss({
+        title: 'Fenils Blog',
+        description: '',
+        site: context.site,
+        items: sortedBlogs.map((blog) => ({
+          title: blog.data.title,
+          pubDate: blog.data.publishedOn,
+          description: '',
+          link: `/blog/${blog.data.slug}/`,
+        })),
+    });
 }
