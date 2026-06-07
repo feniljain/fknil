bc06336bbf80635e81ec1af0d3985f545ece04f7
feniljain <fkjainco@gmail.com>
2026-06-07 12:30:50 +0530

    feat: wander console
    https://codeberg.org/susam/wander/

---
+++ src/scripts/wander.js
@@ -0,0 +1,37 @@
+const wander = {
+   // Other Wander consoles that visitors can reach from my console.
+   consoles: [
+       'https://susam.net/wander/',
+       'https://matkelly.com/wander/',
+       'https://juleskourelakos.com/wander/',
+       'https://fractaldragon.net/wander/',
+       'https://foodbark.io/wander/',
+   ],
+
+   // My favourite websites and pages I recommend to the Wander community.
+   pages: [
+       'https://yashgarg.dev/',
+       'https://msfjarvis.dev/',
+       'https://wantguns.dev/',
+       'https://xkcd.com/',
+       'https://thedailywtf.com/',
+       'https://solar.lowtechmagazine.com/',
+       'https://blr.indiewebclub.org/'
+       'https://matklad.github.io/'
+       'https://eli.thegreenplace.net/',
+       'https://ratfactor.com/',
+       'https://muratbuffalo.blogspot.com/',
+       'https://www.bitsxpages.com/',
+       'https://www.allthingsdistributed.com/',
+       'https://brooker.co.za/',
+       'https://www.potaroo.net/',
+       'https://transactional.blog/',
+       'https://pagedout.institute/',
+   ],
+
+   // Websites and consoles to ignore.  My console will never fetch
+   // consoles or web pages whose URLs match the following patterns.
+   ignore: [],
+};
+
+export default wander;
